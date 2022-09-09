13af31645ce878a70d78df6630590fc2449b638a
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-09-09 14:21:24 +0700

    🔨 Update the enviroment variable

--- src/Main.ts
+++ src/Main.ts
@@ -1,7 +1,12 @@
 import { FastifyInstance } from "fastify";
+import dotenv from 'dotenv';
 import AppBuilder from "./Builder/AppBuilder";
 
+dotenv.config();
+
 const server = new AppBuilder({ logger: true });
+const port: number = Number.parseInt(process.env.APP_PORT as string) ?? 3000;
+const host: string = process.env.APP_HOST ?? '0.0.0.0';
 
 const app: FastifyInstance = server.build();
-app.listen({ port: 3000 });
+app.listen({ port: port, host: host });
