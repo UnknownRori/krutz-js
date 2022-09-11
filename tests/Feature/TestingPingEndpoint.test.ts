67fc2fe4ae9448a757f34e46b7dc60324de02de8
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-09-12 04:11:59 +0700

    🔨 Should serialize the data first before matching

--- tests/Feature/TestingPingEndpoint.test.ts
+++ tests/Feature/TestingPingEndpoint.test.ts
@@ -5,7 +5,8 @@ test('Test ping endpoint', async () => {
 
     const fastify = app.getFastifyInstance();
     const response = await fastify.inject({ 'method': 'GET', url: '/api/ping' });
+    const expectedResponseBody = JSON.stringify({ 'message': 'Pong!' });
 
-    expect(response.body).toEqual({ 'message': 'Pong!' });
+    expect(response.body).toEqual(expectedResponseBody);
     expect(response.statusCode).toBe(200);
 });
