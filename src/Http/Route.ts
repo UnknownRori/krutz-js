6cfe51cac9b4961a85a177d74165532505cc1b72
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-09-14 04:28:44 +0700

    🔨 Added documentation

--- src/Http/Route.ts
+++ src/Http/Route.ts
@@ -19,51 +19,98 @@ export default class Route {
         this.current = '';
     }
 
+    /**
+     * Add HEAD HTTP Endpoint
+     * @param url string
+     * @param closure RouteClosure
+     * @returns this
+     */
     public head(url: string, closure: RouteClosure): this {
         this.current = url;
         this.HEAD.set(this.combinePrefixAndURL(url), closure);
         return this;
     }
-
+    /**
+     * Add GET HTTP Endpoint
+     * @param url string
+     * @param closure RouteClosure
+     * @returns this
+     */
     public get(url: string, closure: RouteClosure): this {
         this.current = url;
         this.GET.set(this.combinePrefixAndURL(url), closure);
         return this;
     }
 
+    /**
+     * Add POST HTTP Endpoint
+     * @param url string
+     * @param closure RouteClosure
+     * @returns this
+     */
     public post(url: string, closure: RouteClosure): this {
         this.current = url;
         this.POST.set(this.combinePrefixAndURL(url), closure);
         return this;
     }
 
+    /**
+     * Add PUT HTTP Endpoint
+     */
     public put(url: string, closure: RouteClosure): this {
         this.current = url;
         this.PUT.set(this.combinePrefixAndURL(url), closure);
         return this;
     }
 
+    /**
+     * Add PATCH HTTP Endpoint
+     * @param url string
+     * @param closure RouteClosure
+     * @returns this
+     */
     public patch(url: string, closure: RouteClosure): this {
         this.current = url;
         this.PATCH.set(this.combinePrefixAndURL(url), closure);
         return this;
     }
 
+    /**
+     * Add DELETE HTTP Endpoint
+     * @param url string
+     * @param closure RouteClosure
+     * @returns this
+     */
     public delete(url: string, closure: RouteClosure): this {
         this.current = url;
         this.DELETE.set(this.combinePrefixAndURL(url), closure);
         return this;
     }
 
+    /**
+     * Add Current Route a Name
+     * @param name string
+     * @returns this
+     */
     public name(name: string): this {
         this.NAME.set(name, this.current);
         return this;
     }
 
+    /**
+     * Get the route using name as a context
+     * @param name string
+     * @returns string|undefined
+     */
     public getRoute(name: string): string | undefined {
         return this.NAME.get(name);
     }
 
+    /**
+     * Combine the URL with Route Prefix
+     * @param url string
+     * @returns string
+     */
     protected combinePrefixAndURL(url: string): string {
         return this.prefix + url;
     }
