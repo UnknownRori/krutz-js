ca0ce873a55fe9757320a88efd8edbbb8717b9bc
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-09-19 09:38:28 +0700

    🔨 Implement RandomString Service

---
+++ src/Services/RandomStringService.ts
@@ -0,0 +1,34 @@
+import { randomUUID } from "crypto";
+import prisma from "./Prisma";
+
+export default class RandomString {
+
+    constructor(protected limitLoop = 4) {
+    }
+    /**
+     * Generate random text
+     * @param limit number
+     * @param counter number
+     * @returns string
+     * @throw Error
+     */
+    async generate(counter = 0): Promise<string> {
+        const randomStringCollection = randomUUID().split('-').filter((val) => val.length < 10);
+        const index = Math.floor(Math.random() * randomStringCollection.length);
+        const randomString = randomStringCollection[index];
+
+        const dbResult = await prisma.url.findFirst({
+            where: {
+                short: randomString
+            }
+        });
+
+        if (counter >= this.limitLoop)
+            throw new Error("Exceeding the limit");
+
+        if (dbResult != null)
+            return this.generate(counter++);
+
+        return randomString;
+    }
+}
