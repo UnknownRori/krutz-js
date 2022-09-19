02e4d618efbf38a3d6f6a6b8d42d50aaefdd133c
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-09-19 09:39:01 +0700

    🔨 Implement shorting algorithm on endpoint

--- src/Controller/UriShortController.ts
+++ src/Controller/UriShortController.ts
@@ -1,16 +1,42 @@
 import { FastifyReply, FastifyRequest } from "fastify";
+import UriShortRequest from "../Types/UriShortRequest";
+import prisma from "../Services/Prisma";
+import RandomString from "../Services/RandomStringService";
 
-// Todo! Properly implement shorten the uri
 const UriShortController = async (request: FastifyRequest, reply: FastifyReply) => {
-    reply.status(201);
-    reply.send({
-        code: 201,
-        message: 'URI successfully shorten!',
-        uri: {
-            uri: (request.body as UriShortResponse).uri,
-            short: 'asdijasodjaso'
-        }
-    });
+    const requestBody = request.body as UriShortRequest;
+
+    try {
+        const RandomStringService = new RandomString();
+
+        const newUrl = await prisma.url.create({
+            "data": {
+                short: await RandomStringService.generate(),
+                url: requestBody.uri
+            }
+        });
+
+        reply
+            .status(201)
+            .send({
+                code: 201,
+                message: 'URI successfully shorten!',
+                uri: {
+                    uri: newUrl.url,
+                    short: newUrl.short,
+                    createdAt: newUrl.createdAt
+                }
+            });
+    } catch (e) {
+        console.error(e);
+        reply
+            .status(508)
+            .send({
+                code: 508,
+                message: 'Try again later',
+                explain: 'Too many shorting attempt on the server'
+            });
+    }
 };
 
 export default UriShortController;
