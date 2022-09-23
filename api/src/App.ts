3ee2849c4711b73506896fd5867aa7df548ee83c
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-09-23 10:26:11 +0700

    🔨 Extract Enviroment

--- api/src/App.ts
+++ api/src/App.ts
@@ -1,7 +1,7 @@
 import { FastifyInstance, FastifyServerOptions } from "fastify";
 import FastifyBuilder from "./Builder/FastifyBuilder";
-import dotenv from 'dotenv';
 import router from "./Router/Router";
+import env from "./Services/Enviroment";
 
 /**
  * Bootstraping the Krutz Application
@@ -12,12 +12,11 @@ export default class App {
     protected host: string;
 
     constructor(opts: FastifyServerOptions = {}) {
-        dotenv.config();
-
         const builder = new FastifyBuilder(opts, router);
         this.instance = builder.build();
-        this.port = Number.parseInt(process.env.APP_PORT as string) ?? 3000;
-        this.host = process.env.APP_HOST ?? '0.0.0.0';
+
+        this.host = env.APP_HOST;
+        this.port = env.APP_PORT;
     }
 
     /**
---
+++ api/src/Services/Enviroment.ts
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+
+class Enviroment {
+    public APP_HOST: string;
+    public APP_PORT: number;
+
+    constructor() {
+        dotenv.config();
+        this.APP_HOST = process.env.APP_HOST ?? '0.0.0.0';
+        this.APP_PORT = Number.parseInt(process.env.APP_PORT as string) ?? 3000;
+    }
+}
+
+const env = new Enviroment();
+
+export default env;
