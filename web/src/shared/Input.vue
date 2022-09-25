6f9924c8dce45adfa79653babe22de1a2a36fe48
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-09-26 02:39:30 +0700

    🔨 Implement custom input

---
+++ web/src/shared/Input.vue
@@ -0,0 +1,17 @@
+<template>
+    <input :type='type' :value='modelValue'
+        @input='$emit("update:modelValue", ($event.target as HTMLInputElement).value)' :placeholder='placeholder'>
+</template>
+
+
+<script setup lang='ts'>
+defineProps<{
+    type: string,
+    placeholder: string,
+    modelValue: string
+}>();
+
+defineEmits<{
+    (e: 'update:modelValue', value: string): void
+}>();
+</script>
